cb9e1f85f290aeea1c452ef73af352505c5c399a
sukith0 <alexboubkeur@hotmail.fr>
2023-11-23 09:02:30 +0100

    commit

---
+++ truc/don.js
@@ -0,0 +1,41 @@
+
+/*
+let nomUtilisateur
+let ageUtilisateur
+
+nomUtilisateur= prompt("C'est quoi ton blase ?")
+ageUtilisateur= prompt("T'es vieux comment ?")
+
+
+alert(`bonjour ${nomUtilisateur}, vous avez ${ageUtilisateur} ans!`)*/
+
+let randomTarget=Math.floor(Math.random()*100)+1
+console.log(randomTarget)
+
+let userNumber=null
+let count=0
+
+
+/*if(randomTarget==userNumber) {
+
+     alert("Au nom c'est ça")}*/
+
+
+     /*utile
+
+while(userNumber!=randomTarget) { userNumber=prompt("Devine le nombre entre 1 et 100 mon gars")
+count++
+
+    if (isNaN(userNumber)) {
+    alert("il faut un chiffre frérot tu crois arnaquer qui là")
+    }  
+    else if(randomTarget<userNumber) {
+    alert("trop grand l'équipage")
+    }
+    else if (randomTarget>userNumber) {
+    alert("trop petit man poteeee aaaa")
+    } 
+    else {
+    alert(`Au nom c'est ça, t'a trouvé en ${count} coups mon bg à la crème anglaise`)
+    }
+}*/
